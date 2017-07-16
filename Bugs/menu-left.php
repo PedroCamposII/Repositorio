a8ab6aeb91d6c819ace644bd66a528f15993d007
PedroCamposII <J.Pedro_Campos@outlook.com>
2017-07-15 21:28:43 -0300

    (Sem)

---
+++ Bugs/menu-left.php
@@ -0,0 +1,21 @@
+<?php
+    require("head.php");
+?>
+<!DOCTYPE>
+<html>
+    <head></head>
+    <body>
+        <input type="checkbox" id="check" class="disp-n">
+        <label id="icone" for="check"> <img src="_images/menu-alt-512.png" width="50px" height="50px"> </label>
+        <div class="barra">
+            <nav class="nav-mn">
+                <ul>
+                    <li class="link-mn"><a href=""class="dec-none">Inicio</a></li>
+                    <li class="link-mn"><a href="" class="dec-none">Softwares</a></li>
+                    <li class="link-mn"><a href="" class="dec-none">Assistência</a></li>
+                    <li class="link-mn"><a href="" class="dec-none">Ajuda</a></li>
+                </ul>          
+            </nav>
+        </div>
+    </body>
+</html>
